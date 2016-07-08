8e96280f4b2bd0baedc12d66e1ed61f51a54158b
Euphonux <zz1322@nyu.edu>
2016-07-08 19:46:31 -0400

    changed into new way of controlling, make more sense.

--- Assets/Scripts/TwoDimensionalController.cs
+++ Assets/Scripts/TwoDimensionalController.cs
@@ -6,7 +6,7 @@ public class TwoDimensionalController : MonoBehaviour {
 	public float speed = 3f;
 	Rigidbody2D _rigidBody;
 	public float anglarSpeed = 90f; //degree per seconds
-	List<Vector2> inputStack;
+	List<int> inputStack;
 
 
 
@@ -14,8 +14,8 @@ public class TwoDimensionalController : MonoBehaviour {
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody2D> ();
 		_rigidBody.velocity = transform.up;
-		inputStack = new List<Vector2> ();
-		inputStack.Add (Vector3.zero);
+		inputStack = new List<int> ();
+		inputStack.Add (0);
 	}
 	
 	// Update is called once per frame
@@ -24,24 +24,75 @@ public class TwoDimensionalController : MonoBehaviour {
 		//Debug.Log (_rigidBody.velocity);
 		
 		Vector3 direction = Vector3.zero;
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			inputStack.Add(Vector2.left);
-		} else if (Input.GetKeyDown (KeyCode.RightArrow))
-			inputStack.Add(Vector2.right);
-		else if (Input.GetKeyDown (KeyCode.UpArrow))
-			inputStack.Add(Vector2.up);
-		else if (Input.GetKeyDown (KeyCode.DownArrow))
-			inputStack.Add(Vector2.down);
-		else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
-			inputStack.RemoveAll(element => element == Vector2.left);
-		} else if (Input.GetKeyUp (KeyCode.RightArrow))
-			inputStack.RemoveAll(element => element == Vector2.right);
-		else if (Input.GetKeyUp (KeyCode.UpArrow))
-			inputStack.RemoveAll(element => element == Vector2.up);
-		else if (Input.GetKeyUp (KeyCode.DownArrow))
-			inputStack.RemoveAll(element => element == Vector2.down);
-		direction = inputStack [inputStack.Count - 1];
-		Debug.Log (direction);
+		int currentDirect = inputStack[inputStack.Count - 1];
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if (currentDirect != 1)
+				inputStack.Add(1);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if (currentDirect != 2)
+				inputStack.Add(2);
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if (currentDirect != 3)
+				inputStack.Add(3);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			if (currentDirect != 4)
+			inputStack.Add(4);
+		}
+
+		while (inputStack.Count > 1) 
+		{
+			currentDirect = inputStack[inputStack.Count - 1];
+			if (currentDirect == 1)
+			{
+				if (!Input.GetKey(KeyCode.LeftArrow))
+					inputStack.RemoveAt(inputStack.Count - 1);
+				else
+					break;
+			}
+			if (currentDirect == 2)
+			{
+				if (!Input.GetKey(KeyCode.RightArrow))
+					inputStack.RemoveAt(inputStack.Count - 1);
+				else
+					break;
+			}
+			if (currentDirect == 3)
+			{
+				if (!Input.GetKey(KeyCode.UpArrow))
+					inputStack.RemoveAt(inputStack.Count - 1);
+				else
+					break;
+			}
+			if (currentDirect == 4)
+			{
+				if (!Input.GetKey(KeyCode.DownArrow))
+					inputStack.RemoveAt(inputStack.Count - 1);
+				else
+					break;
+			}
+
+		}
+		currentDirect = inputStack[inputStack.Count - 1];
+
+		if (currentDirect == 0)
+			direction = Vector2.zero;
+		else if (currentDirect == 1)
+			direction = Vector2.left;
+		else if (currentDirect == 2)
+			direction = Vector2.right;
+		else if (currentDirect == 3)
+			direction = Vector2.up;
+		else if (currentDirect == 4)
+			direction = Vector2.down;
+
+
 		_rigidBody.velocity = Quaternion.Euler (0f, 0f, isTurnLeft(_rigidBody.velocity, direction) * anglarSpeed * Time.deltaTime * 75f) * _rigidBody.velocity.normalized * speed;
 
 	}
