2870e07ec01fd85915845a7d367c0a5b2c24ab09
gamesketches <sam.von.ehren@gmail.com>
2016-07-10 14:44:00 -0400

    Created script to randomly populate obstacles on level

---
+++ Assets/Scripts/GenLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] obstacles = Resources.LoadAll<GameObject>("prefabs");
+		foreach(GameObject obstacle in obstacles) {
+			for(int i = 0; i < 3; i++) {
+				Instantiate(obstacle, new Vector3((Random.value * 12f) - 12f, (Random.value * 12f) - 12f, 0), Quaternion.identity);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
