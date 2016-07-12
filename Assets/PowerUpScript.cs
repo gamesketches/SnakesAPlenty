c54c290ea1c7926cd885458b101dd1adb79bdbf4
gamesketches <sam.von.ehren@gmail.com>
2016-07-12 00:02:18 -0400

    added power ups and a script to for collisions

---
+++ Assets/PowerUpScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player"){
+			Debug.Log("Player entered " + gameObject.name);
+			}
+		else{
+			Debug.Log(other.tag);
+		}
+	}
+}
