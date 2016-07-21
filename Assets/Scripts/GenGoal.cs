e92e99413cb4148793e2843b9cef9d3392eb7e9b
gamesketches <sam.von.ehren@gmail.com>
2016-07-20 23:01:04 -0400

    Added win text to all players

--- Assets/Scripts/GenGoal.cs
+++ Assets/Scripts/GenGoal.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GenGoal : MonoBehaviour {
 
@@ -11,5 +12,12 @@ public class GenGoal : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player") {
+			Text winText = other.gameObject.transform.parent.GetComponentInChildren<Text>();
+			winText.text = "YOU WIN!!";
+		}
 	}
 }
